8f327d3af871e11df90bb5e53833ddbc939a2a3d
Renan Ricoldi Frois Pedro <renan.ricoldi.frois@gmail.com>
2021-12-08 00:58:48 -0300

    feat: finished base transformations
    - fix: ds and dw database
    - fix: especificacao ds

--- 01. CriaDS_pentaho.sql
+++ 01. CriaDS_pentaho.sql
@@ -3,7 +3,7 @@
 --
 DROP TABLE IF EXISTS D_vendas;
 DROP TABLE IF EXISTS D_BDI;
-DROP TABLE IF EXISTS D_ESPECIFICACAO;
+DROP TABLE IF EXISTS D_ESPECIFICACAO CASCADE;
 DROP TABLE IF EXISTS D_INDOPC;
 DROP TABLE IF EXISTS D_NOMEACAO;
 
@@ -59,42 +59,67 @@ CREATE TABLE D_Data(
 
 create table D_BDI(
 	codigo_bdi varchar(2) primary key not null,
-	nome_bdi varchar(55) not null,
+	nome_bdi varchar(90) not null,
 	lindata date not null,
 	linorig varchar(50) not null
 );
-
-create index IX_Codigo_BDI on D_BDI (codigo_bdi);
 
 -- tabela D_ESPECIFICACAO -----------------
 
 create table D_ESPECIFICACAO(
 	Sigla_especificacao varchar(15) primary key not null,
-	Nome_especificacao varchar(55) not null,
+	Nome_especificacao varchar(90) not null,
 	lindata date not null,
 	linorig varchar(50) not null
 );
 
-create index IX_Sigla_Especificacao on D_ESPECIFICACAO (sigla_especificacao);
 
 -- tabela D_INDOPC ------------------
 create table D_INDOPC(
-	Codigo_INDOPC int primary key not null,
+	Codigo_INDOPC varchar(5) primary key not null,
 	Sigla_INDOPC varchar(5) not null,
-	Nome_INDOPC varchar(45) not null,
+	Nome_INDOPC varchar(90) not null,
 	lindata date not null,
 	linorig varchar(50) not null
 );
 
-create index IX_Codigo_INDOPC on D_INDOPC (Codigo_INDOPC);
+-- tabela D_TPMERC ------------------
+
+create table D_TPMERC(
+	Codigo_TPMERC varchar(5) primary key not null,
+	Nome_TPMERC varchar(90) not null,
+	lindata date not null,
+	linorig varchar(50) not null
+);
 
 -- tabela D_NOMEACAO ------------------
 
-create table D_NOMEACAO(
-	Codigo_TPMERC int primary key not null,
-	Nome_TPMERC varchar(45) not null,
+create table D_NOME_ACAO(
+	Codigo_ACAO varchar(12) primary key not null,
+	Nome_ACAO varchar(90) not null,
+	Sigla_especificacao varchar(15) not null,
 	lindata date not null,
-	linorig varchar(50) not null
+	linorig varchar(50) not null,
+	
+	constraint fk_d_nome_acao_especificacao foreign key (Sigla_especificacao)
+	references D_ESPECIFICACAO
+	on delete cascade
 );
 
-create index IX_Codigo_TPMERC on D_NOMEACAO (Codigo_TPMERC);
+create table F_B3(
+	Data date not null,
+	Codigo_TPMERC varchar(5) not null,
+	Codigo_INDOPC varchar(5) not null,
+	Codigo_ACAO varchar(12) not null,
+	codigo_bdi varchar(2) not null,
+	lindata date not null,
+	linorig varchar(50) not null,
+
+	constraint pk_f_b3 primary key (
+		Data,
+		Codigo_TPMERC,
+		Codigo_INDOPC,
+		Codigo_ACAO,
+		codigo_bdi
+	)
+);
